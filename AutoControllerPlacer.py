a34a0a42c61eb619e2003c2467dda99ed68fb8bd
raka_07 <raka3dprogramming@gmail.com>
2026-02-04 20:07:22 -0600

    Create AutoControllerPlacer.py
    Technical Direction

---
+++ AutoControllerPlacer.py
@@ -0,0 +1,8 @@
+import maya.cmds as mc
+selection = mc.ls(sl = True)
+for item in selection:
+    NewController = mc.circle(n = item.replace("jt_drv", "ac"))
+    NewGrp = mc.group(NewController, n = item.replace("jt_drv", "ac_drv") + "_grp")
+    OffsetGrp = mc.group(NewController, n = NewGrp.replace("grp", "grp_offset"))
+    mc.matchTransform(NewGrp, item)
+    mc.matchTransform(OffsetGrp, item)
